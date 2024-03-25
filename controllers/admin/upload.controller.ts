e46aa78c34545eedadab77a58f66422627c46cbf
Huynh Nguyen <yugivip277@gmail.com>
2024-03-25 14:25:45 +0700

    deploy to vercel

--- controllers/admin/upload.controller.ts
+++ controllers/admin/upload.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 
 export const upload = async (req: Request, res: Response) => {
-  console.log(req.body)
   res.json({
     location: req.body.file
   })
