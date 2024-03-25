6f5dd082ef6b9c8bcf0ec3b22f5ea2fb1f68d0d1
Huynh Nguyen <yugivip277@gmail.com>
2024-03-25 14:29:29 +0700

    fix precommit

--- controllers/admin/upload.controller.ts
+++ controllers/admin/upload.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 export const upload = async (req: Request, res: Response) => {
+  console.log(req.body);
   res.json({
     location: req.body.file
   })
